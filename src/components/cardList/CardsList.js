e873a9f6069a219ab7b017d6e11c87095d2962d3
gadaharon <gadaharon91@gmail.com>
2020-12-16 07:39:06 +0200

    move CardList to folder

--- src/components/cardList/CardsList.js
+++ src/components/cardList/CardsList.js
@@ -2,21 +2,23 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { cloneDeep } from 'lodash';
 import Pagination from '@material-ui/lab/Pagination';
+import './style.css';
 
-import Card from './Card';
-
-const NUM_OF_CARDS_PER_PAGE = 10;
+const NUM_OF_CARDS_PER_PAGE = 30;
 
 const CardsList = () => {
   const [cardsList, setCardsList] = useState([]);
   const [cardsPageData, setCardsPageData] = useState([]);
 
-  useEffect(async () => {
+  const fetchCardsList = async () => {
     const { data } = await axios.get(
       'https://db.ygoprodeck.com/api/v7/cardinfo.php'
     );
-    // console.log(data);
     setCardsList(data.data);
+  };
+
+  useEffect(() => {
+    fetchCardsList();
   }, []);
 
   useEffect(() => {
@@ -35,9 +37,18 @@ const CardsList = () => {
   return (
     <div>
       <h3>Cards list</h3>
-      {cardsPageData.map((card, index) => (
-        <Card key={index} cardData={card} />
-      ))}
+      <div className="card-list-container">
+        {cardsPageData.map((card, index) => (
+          <div className="card-list-item">
+            <div className="card-list-item-label">{card.name}</div>
+            <img
+              className="card-list-item-image"
+              src={card.card_images[0].image_url}
+              alt="card"
+            />
+          </div>
+        ))}
+      </div>
       <Pagination
         onChange={onPageChange}
         count={Math.floor(cardsList.length / NUM_OF_CARDS_PER_PAGE)}
